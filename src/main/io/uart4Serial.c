6d432d1eac3808e2e2083b8d8ab1cb32c840d2cc
Neuroflight <>
2022-04-27 14:03:49 -0400

    switching to uart3 for xbee for nf1 rather than nf2

--- src/main/io/uart4Serial.c
+++ src/main/io/uart4Serial.c
@@ -5,7 +5,7 @@ serialPort_t *uart4Serial = NULL;
 
 serialPort_t* getUART4() {
 	if(uart4Serial == NULL) {
-		uart4Serial = openSerialPort(SERIAL_PORT_UART4, FUNCTION_BLACKBOX, NULL, NULL, 115200, MODE_RXTX, SERIAL_HW_FLOW_CTRL);
+		uart4Serial = openSerialPort(SERIAL_PORT_USART3, FUNCTION_BLACKBOX, NULL, NULL, 115200, MODE_RXTX, SERIAL_HW_FLOW_CTRL);
 		if(millis() < 10000) // waiting 10 seconds so that the xbee connects since drone power on
 			delay(10000 - millis());
 		serialWrite(uart4Serial, ' '); 
